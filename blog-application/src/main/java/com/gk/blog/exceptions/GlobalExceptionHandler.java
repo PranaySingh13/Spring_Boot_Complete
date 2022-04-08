f880b3a08de8ba43b977e1d22ce438d3dca2aac6
Pranay Singh <pranay.singh.1393@gmail.com>
2022-04-09 05:00:59 +0530

    Global Exception Handler
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- blog-application/src/main/java/com/gk/blog/exceptions/GlobalExceptionHandler.java
+++ blog-application/src/main/java/com/gk/blog/exceptions/GlobalExceptionHandler.java
@@ -10,6 +10,10 @@ import com.gk.blog.payloads.ApiResponse;
 @RestControllerAdvice // It handles exception globally overall Restcontroller APis.
 public class GlobalExceptionHandler {
 
+	/*
+	 * The @ExceptionHandler is an annotation used to handle the specific exceptions
+	 * and sending the custom responses to the client.
+	 */
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
 		String message = ex.getMessage();
