15f18788c2033bacc751b865080bdf903d4ca193
Pranay Singh <pranay.singh.1393@gmail.com>
2022-04-09 04:57:05 +0530

    API Response
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ blog-application/src/main/java/com/gk/blog/payloads/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.gk.blog.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+	
+	private String message;
+	private boolean success;
+	
+
+}
