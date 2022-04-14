1e1290529b5005f4f698194f6d189a5cebdba083
Pranay Singh <pranay.singh.1393@gmail.com>
2022-04-14 15:38:24 +0530

    Blog Application Post Entity Mapping with Category and User
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- blog-application/src/main/java/com/gk/blog/entity/Category.java
+++ blog-application/src/main/java/com/gk/blog/entity/Category.java
@@ -1,10 +1,16 @@
 package com.gk.blog.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -29,4 +35,7 @@ public class Category {
 
 	@Column
 	private String categoryDescription;
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 }
---
+++ blog-application/src/main/java/com/gk/blog/entity/Post.java
@@ -0,0 +1,50 @@
+package com.gk.blog.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "post")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postId;
+
+	@Column(name = "post_title",length = 100,nullable = false)
+	private String title;
+
+	@Column(name = "post_content",length = 10000,nullable = false)
+	private String content;
+
+	@Column
+	private Date addedDate;
+
+	@Column
+	private String imageName;
+	
+	@ManyToOne
+	@JoinColumn(name="category_id")
+	private Category category;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+}
