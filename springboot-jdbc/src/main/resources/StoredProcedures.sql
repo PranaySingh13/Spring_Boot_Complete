3511ccf83a65ca1f9ec2a1bea944604ab5d64539
Pranay Singh <pranay.singh.1393@gmail.com>
2022-04-20 15:57:04 +0530

    Stored Procedures SQL File
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- springboot-jdbc/src/main/resources/StoredProcedures.sql
+++ springboot-jdbc/src/main/resources/StoredProcedures.sql
@@ -1,61 +1,66 @@
 /*---------------------------------------*/
-DROP PROCEDURE IF EXISTS `springbootjdbcdb.insertUser`;
+USE `springbootjdbcdb`;
+DROP procedure IF EXISTS `insertUser`;
 
 DELIMITER $$
-CREATE PROCEDURE springbootjdbcdb.insertUser(IN uid INTEGER(11),IN uname VARCHAR(255),IN uage INTEGER(11),IN ucity VARCHAR(255))
+USE `springbootjdbcdb`$$
+CREATE PROCEDURE insertUser(IN uid INTEGER(11),IN uname VARCHAR(255),IN uage INTEGER(11),IN ucity VARCHAR(255))
 BEGIN
 insert into USER values(uid, uname, uage, ucity);
-END $$
+END$$
+
 DELIMITER ;
 
-CALL springbootjdbcdb.insertUser(@uid,@uname,@uage,@ucity);
-
 /*---------------------------------------*/
-DROP PROCEDURE IF EXISTS `springbootjdbcdb.getUser`;
+USE `springbootjdbcdb`;
+DROP procedure IF EXISTS `getUser`;
 
 DELIMITER $$
-CREATE PROCEDURE springbootjdbcdb.getUser(IN no INTEGER(11),OUT uid INTEGER(11),OUT uname VARCHAR(255),OUT uage INTEGER(11),OUT ucity VARCHAR(255))
+USE `springbootjdbcdb`$$
+CREATE PROCEDURE getUser(IN no INTEGER(11),OUT uid INTEGER(11),OUT uname VARCHAR(255),OUT uage INTEGER(11),OUT ucity VARCHAR(255))
 BEGIN
 select id, name, age, city into uid, uname, uage, ucity from USER where id= no;
-END $$
+END$$
+
 DELIMITER ;
 
 /*---------------------------------------*/
-DROP PROCEDURE IF EXISTS `springbootjdbcdb.getAllUsers`;
+USE `springbootjdbcdb`;
+DROP procedure IF EXISTS `getAllUsers`;
 
 DELIMITER $$
-CREATE PROCEDURE springbootjdbcdb.getAllUsers(OUT uid INTEGER(11),OUT uname VARCHAR(255),OUT uage INTEGER(11),OUT ucity VARCHAR(255))
+USE `springbootjdbcdb`$$
+CREATE PROCEDURE getAllUsers(OUT uid INTEGER(11),OUT uname VARCHAR(255),OUT uage INTEGER(11),OUT ucity VARCHAR(255))
 BEGIN
 select id, name, age, city into uid, uname, uage, ucity from USER;
-END $$
+END$$
+
 DELIMITER ;
 
 /*---------------------------------------*/
-DROP PROCEDURE IF EXISTS `springbootjdbcdb.updateUser`;
+USE `springbootjdbcdb`;
+DROP procedure IF EXISTS `updateUser`;
 
 DELIMITER $$
-CREATE PROCEDURE springbootjdbcdb.updateUser(IN uid INTEGER(11),IN uname VARCHAR(255),IN uage INTEGER(11),IN ucity VARCHAR(255))
+USE `springbootjdbcdb`$$
+CREATE PROCEDURE updateUser(IN uid INTEGER(11),IN uname VARCHAR(255),IN uage INTEGER(11),IN ucity VARCHAR(255))
 BEGIN
 update USER set name = uname, age = uage, city = ucity where id = uid;
-END $$
+END$$
+
 DELIMITER ;
 
 /*---------------------------------------*/
-DROP PROCEDURE IF EXISTS `springbootjdbcdb.deleteUser`;
+USE `springbootjdbcdb`;
+DROP procedure IF EXISTS `deleteUser`;
 
 DELIMITER $$
-CREATE PROCEDURE springbootjdbcdb.deleteUser(IN uid INTEGER(11))
+USE `springbootjdbcdb`$$
+CREATE PROCEDURE deleteUser(IN uid INTEGER(11))
 BEGIN
 delete from USER where id = uid;
-END $$
+END$$
+
 DELIMITER ;
 
 /*---------------------------------------*/
-DROP PROCEDURE IF EXISTS `getSalary`;
-
-DELIMITER $$
-CREATE PROCEDURE getSalary(IN eid INTEGER(11),OUT esal FLOAT(10,2))
-BEGIN
-select EMPSALARY into esal from EMPLOYEE where EMPID = eid;
-END $$
-DELIMITER ;
