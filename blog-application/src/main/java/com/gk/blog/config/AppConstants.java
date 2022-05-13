a3c25777e67dd38103c60e029d7388b8a83ae75f
Pranay Singh <pranay.singh.1393@gmail.com>
2022-05-14 04:52:50 +0530

    Blog Application App Constants
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- blog-application/src/main/java/com/gk/blog/config/AppConstants.java
+++ blog-application/src/main/java/com/gk/blog/config/AppConstants.java
@@ -6,5 +6,13 @@ public class AppConstants {
 	public static final String PAGE_SIZE = "10";
 	public static final String SORT_BY = "postId";
 	public static final String SORT_DIRECTION = "asc";
+	
+	public static final Integer ROLE_ADMIN = 1;
+	public static final Integer ROLE_USER = 2;
+	
+	public static final long JWT_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60;
+	public static final String SECRET_KEY = "jwtTokenKey";
+	public static final String TOKEN_PREFIX = "Bearer ";
+	public static final String HEADER_STRING = "Authorization";
 
 }
