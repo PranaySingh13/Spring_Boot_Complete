5f04ba3f4840e28b954a5acf35c30e58f12ded39
Pranay Singh <pranay.singh.1393@gmail.com>
2022-05-20 04:08:15 +0530

    Blog Application Swagger Configuration with Spring Security
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- blog-application/src/main/java/com/gk/blog/config/SwaggerConfig.java
+++ blog-application/src/main/java/com/gk/blog/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.gk.blog.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,14 +10,34 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	private List<SecurityScheme> apiKeys() {
+		return Arrays.asList(new ApiKey("JWT", AUTHORIZATION_HEADER, "header"));
+	}
+
+	private List<SecurityContext> securityContexts() {
+		return Arrays.asList(SecurityContext.builder().securityReferences(securityReferences()).build());
+	}
+
+	private List<SecurityReference> securityReferences() {
+		AuthorizationScope scope = new AuthorizationScope("global", "Access Everything");
+		return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[] { scope }));
+	}
+
 	/**
 	 * A builder which is intended to be the primary interface into the Springfox framework.
 	 * Provides sensible defaults and convenience methods for configuration.
@@ -22,6 +45,7 @@ public class SwaggerConfig {
 	//Docket class to inform swagger that we require documentation of type 2 
 	@Bean
 	public Docket swaggerConfiguration() {
+		
 
 		return new Docket(DocumentationType.SWAGGER_2)
 				.groupName("Blog Application")
@@ -29,7 +53,9 @@ public class SwaggerConfig {
 				.paths(PathSelectors.ant("/**"))//url for which controller we require documentation
 				.apis(RequestHandlerSelectors.basePackage("com.gk.blog"))
 				.build()
-				.apiInfo(apiInfo());
+				.apiInfo(apiInfo())
+				.securityContexts(securityContexts())
+				.securitySchemes(apiKeys());
 	}
 	
 	private ApiInfo apiInfo() {
